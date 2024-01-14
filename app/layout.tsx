c452bd5ab1b721961d8f6ab19947672a632b6bb2
kainy01 <kainykawai@gmail.com>
2024-01-14 15:58:30 +0300

    Fix: fixeing vercel error

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-export const fontSans = FontSans({
+ const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
