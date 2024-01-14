39a89fd8929ff829eaa82303b721fc3d75874ecc
kainy01 <kainykawai@gmail.com>
2024-01-14 16:06:30 +0300

    Fix:trying to fix deply error

---
+++ app/head.jsx
@@ -0,0 +1,10 @@
+export default function CustomHead() {
+    return (
+      <head>
+        <title>Dashbored</title>
+        <meta name="description" content="INZO financial brokerage" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
+    );
+  }
